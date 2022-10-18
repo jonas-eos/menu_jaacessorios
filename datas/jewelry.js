4b807d31db02c6239bb6cea8f040035f07ed2c5b
jonas.silva <jonas.eos@outlook.com>
2022-10-18 07:58:40 -0300

    Removed obsolete images and update jew itens order

--- datas/jewelry.js
+++ datas/jewelry.js
@@ -1,14 +1,32 @@
 export const jewelrys = [
   {
-    name: " Coração vazado",
-    price: "15.00",
-    code: "B745",
+    name: "Argolinha Mini Bolinhas",
+    price: "15.50",
+    code: "B007",
     category: "Brinco"
   },
   {
     name: "Chatton gota blue",
     price: "33.99",
     code: "B175",
+    category: "Brinco"
+  },
+  {
+    name: "Pedraria zirconia morganita",
+    price: "45.99",
+    code: "B176",
+    category: "Brinco"
+  },
+  {
+    name: "Pedraria zirconia frutacor",
+    price: "45.99",
+    code: "B177",
+    category: "Brinco"
+  },
+  {
+    name: " Coração vazado",
+    price: "15.00",
+    code: "B745",
     category: "Brinco"
   },
   {
@@ -24,63 +42,9 @@ export const jewelrys = [
     category: "Brinco"
   },
   {
-    name: "Malha",
-    price: "38.99",
-    code: "C125",
-    category: "Colar"
-  },
-  {
     name: "Corações chapeados prata",
     price: "44.50",
     code: "C043",
-    category: "Colar"
-  },
-  {
-    name: "Corrente elos retangular",
-    price: "24.99",
-    code: "C102",
-    category: "Colar"
-  },
-  {
-    name: "Corações escovados",
-    price: "29.99",
-    code: "CJ815",
-    category: "Conjunto"
-  },
-  {
-    name: "Pingente Teddy",
-    price: "22.99",
-    code: "P962",
-    category: "Pulseira"
-  },
-  {
-    name: "Malha fina",
-    price: "21.50",
-    code: "P947",
-    category: "Pulseira"
-  },
-  {
-    name: "Elos",
-    price: "21.50",
-    code: "P102",
-    category: "Pulseira"
-  },
-  {
-    name: "Medalhas",
-    price: "21.50",
-    code: "P100",
-    category: "Pulseira"
-  },
-  {
-    name: "Corrente perolas e borboleta",
-    price: "21.50",
-    code: "C792",
-    category: "Colar"
-  },
-  {
-    name: "Corrente Elos",
-    price: "24.99",
-    code: "C105",
     category: "Colar"
   },
   {
@@ -90,22 +54,28 @@ export const jewelrys = [
     category: "Colar"
   },
   {
-    name: "Pedraria zirconia frutacor",
-    price: "45.99",
-    code: "B177",
+    name: "Circulo franja cravejada",
+    price: "15.99",
+    code: "C081",
     category: "Brinco"
   },
   {
-    name: "Pedraria zirconia morganita",
-    price: "45.99",
-    code: "B176",
-    category: "Brinco"
+    name: "Corrente elos retangular",
+    price: "24.99",
+    code: "C102",
+    category: "Colar"
   },
   {
-    name: "Verde Com Vermelho",
-    price: "52.50",
-    code: "CJ246",
-    category: "Conjunto"
+    name: "Corrente Elos",
+    price: "24.99",
+    code: "C105",
+    category: "Colar"
+  },
+  {
+    name: "Malha",
+    price: "38.99",
+    code: "C125",
+    category: "Colar"
   },
   {
     name: "Manta",
@@ -114,15 +84,45 @@ export const jewelrys = [
     category: "Colar"
   },
   {
-    name: "Argolinha Mini Bolinhas",
-    price: "15.50",
-    code: "B007",
-    category: "Brinco"
+    name: "Corrente perolas e borboleta",
+    price: "21.50",
+    code: "C792",
+    category: "Colar"
   },
   {
-    name: "Circulo franja cravejada",
-    price: "15.99",
-    code: "C081",
-    category: "Brinco"
+    name: "Verde Com Vermelho",
+    price: "52.50",
+    code: "CJ246",
+    category: "Conjunto"
+  },
+  {
+    name: "Corações escovados",
+    price: "29.99",
+    code: "CJ815",
+    category: "Conjunto"
+  },
+  {
+    name: "Medalhas",
+    price: "21.50",
+    code: "P100",
+    category: "Pulseira"
+  },
+  {
+    name: "Elos",
+    price: "21.50",
+    code: "P102",
+    category: "Pulseira"
+  },
+  {
+    name: "Malha fina",
+    price: "21.50",
+    code: "P947",
+    category: "Pulseira"
+  },
+  {
+    name: "Pingente Teddy",
+    price: "22.99",
+    code: "P962",
+    category: "Pulseira"
   }
 ]
