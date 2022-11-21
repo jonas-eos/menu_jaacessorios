eeca52cfbf32c41f80a44132017859d28caa11f6
jonas.silva <jonas.eos@outlook.com>
2022-11-20 22:44:51 -0300

    resource: product update

--- datas/jewelry.js
+++ datas/jewelry.js
@@ -42,6 +42,162 @@ export const jewelrys = [
     category: "Brinco"
   },
   {
+    name: "Cruz Zirconia - Rosa",
+    price: "20.50",
+    code: "B0801",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Coração - Prata",
+    price: "15.99",
+    code: "B1402",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Ouro",
+    price: "12.99",
+    code: "B1701",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Prata",
+    price: "27.99",
+    code: "B2102",
+    category: "Brinco"
+  },
+  {
+    name: "Argola 03 Fios",
+    price: "29.99",
+    code: "B2301",
+    category: "Brinco"
+  },
+  {
+    name: "Argola Tubo Torcido",
+    price: "29.50",
+    code: "B2901",
+    category: "Brinco"
+  },
+  {
+    name: "Argola Fio Torcido - Prata",
+    price: "29.50",
+    code: "B3102",
+    category: "Brinco"
+  },
+  {
+    name: "Argola Tubo M",
+    price: "28.99",
+    code: "B3301",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Corrente Folheada",
+    price: "27.99",
+    code: "B3401",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Corrente Prata",
+    price: "29.50",
+    code: "B3402",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Nó Ouro",
+    price: "24.50",
+    code: "B3701",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Nó Prata",
+    price: "24.50",
+    code: "B3702",
+    category: "Brinco"
+  },
+  {
+    name: "Margarida Prata",
+    price: "15.90",
+    code: "B3722",
+    category: "Brinco"
+  },
+  {
+    name: "Brinco Boca",
+    price: "8.00",
+    code: "B4702",
+    category: "Brinco"
+  },
+  {
+    name: "Brinco Asa",
+    price: "14.50",
+    code: "B4801",
+    category: "Brinco"
+  },
+  {
+    name: "Argola Tubo P - Prata",
+    price: "29.50",
+    code: "B5102",
+    category: "Brinco"
+  },
+  {
+    name: "Coracão Ouro M",
+    price: "10.99",
+    code: "B5601",
+    category: "Brinco"
+  },
+  {
+    name: "Flor Strass Prata",
+    price: "21.00",
+    code: "B5702",
+    category: "Brinco"
+  },
+  {
+    name: "Flor Zirconia Ouro",
+    price: "17.99",
+    code: "B5801",
+    category: "Brinco"
+  },
+  {
+    name: "Estrela P - Aço Cirurgico",
+    price: "10.50",
+    code: "B7201",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Estrela",
+    price: "17.99",
+    code: "B1101",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola",
+    price: "15.50",
+    code: "B3501",
+    category: "Brinco"
+  },
+  {
+    name: "Coração P Aço Cirurgico",
+    price: "10.99",
+    code: "B7601",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Coração",
+    price: "17.99",
+    code: "B9301",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Ouro",
+    price: "15.50",
+    code: "B9701",
+    category: "Brinco"
+  },
+  {
+    name: "Mini Argola Prata",
+    price: "15.50",
+    code: "B9702",
+    category: "Brinco"
+  },
+  {
     name: "Corrente Elos",
     price: "24.99",
     code: "C105",
@@ -63,6 +219,60 @@ export const jewelrys = [
     name: "Choker tubo navete prata",
     price: "33.99",
     code: "C890P",
+    category: "Colar"
+  },
+  {
+    name: "Kid Coração",
+    price: "21.00",
+    code: "C1501",
+    category: "Colar"
+  },
+  {
+    name: "Gravata Espirito Santo",
+    price: "29.50",
+    code: "C3102",
+    category: "Colar"
+  },
+  {
+    name: "Ponto de Luz Zirconia - Prata",
+    price: "23.99",
+    code: "C3402",
+    category: "Colar"
+  },
+  {
+    name: "Escapulario Ouro",
+    price: "31.00",
+    code: "C7001",
+    category: "Colar"
+  },
+  {
+    name: "Choker Estrela",
+    price: "27.99",
+    code: "C7501",
+    category: "Colar"
+  },
+  {
+    name: "Pingente Cavalo Prata",
+    price: "22.99",
+    code: "C8302",
+    category: "Colar"
+  },
+  {
+    name: "Gravata Cruz",
+    price: "21.00",
+    code: "C8601",
+    category: "Colar"
+  },
+  {
+    name: "Choker Fita 5MM",
+    price: "45.50",
+    code: "C9001",
+    category: "Colar"
+  },
+  {
+    name: "Choker Zirconia Ouro",
+    price: "39.99",
+    code: "C9201",
     category: "Colar"
   },
   {
@@ -94,5 +304,29 @@ export const jewelrys = [
     price: "23.99",
     code: "PG090",
     category: "Piercing"
+  },
+  {
+    name: "Falso Piercing Pressão",
+    price: "16.99",
+    code: "PG2401",
+    category: "Piercing"
+  },
+  {
+    name: "Ponto de Luz Zirconia",
+    price: "21.00",
+    code: "PL101",
+    category: "Pulseira"
+  },
+  {
+    name: "Bolinhas Prata",
+    price: "31.00",
+    code: "PL105",
+    category: "Pulseira"
+  },
+  {
+    name: "Estrela e Perola",
+    price: "31.00",
+    code: "PL107",
+    category: "Pulseira"
   }
 ]
